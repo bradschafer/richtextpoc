b07b579b20f6f79669828dafe5a7c7de799b28b7
bradschafer <brad@theschafergroup.com>
2020-07-29 14:39:42 -0700

    Updates

---
+++ precache-manifest.67f00a055bef6ee242d64436f6910210.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb76920fcff347adc9855f2985c4fd1",
+    "url": "/richtextpoc/index.html"
+  },
+  {
+    "revision": "8b47326ee5598364893c",
+    "url": "/richtextpoc/static/css/2.76c40d3c.chunk.css"
+  },
+  {
+    "revision": "9101ea016790ac7fcf3f",
+    "url": "/richtextpoc/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8b47326ee5598364893c",
+    "url": "/richtextpoc/static/js/2.a8c294a7.chunk.js"
+  },
+  {
+    "revision": "839c7ed7dc6574d9619d419bbed35388",
+    "url": "/richtextpoc/static/js/2.a8c294a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9101ea016790ac7fcf3f",
+    "url": "/richtextpoc/static/js/main.2b8b8cb9.chunk.js"
+  },
+  {
+    "revision": "9eb4bb32fb84576d7efa",
+    "url": "/richtextpoc/static/js/runtime-main.c06990a0.js"
+  }
+]);
